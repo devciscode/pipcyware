c1a441128b4cec633978193f2ac3a934a014ad11
Jason Gomes <jason.gomes@cyware.com>
2023-03-01 16:46:06 -0600

    updated setup.py and created my_cool_test_method

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pipcyware',
+    version='0.0.1',
+    description='Cyware API client package',
+    url='git@github.com:devciscod/pipcyware.git',
+    author='Jason Gomes',
+    author_email='jason.gomes@cyware.com',
+    license='unlicense',
+    packages=['pipcyware'],
+    zip_safe=False
+)
